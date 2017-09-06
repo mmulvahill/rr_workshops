126220227108db51760bdce92d454e6f4507280a
Matt Mulvahill (annapurna) <matthew.mulvahill@ucdenver.edu>
2017-09-06 16:25:33 -0600

    organized folders and started with example Rmd via Rstudio.:

---
+++ R/attendees.R
@@ -0,0 +1,37 @@
+
+
+
+library(readxl)
+library(tidyverse)
+library(qwraps2)
+library(stringr)
+options(width = 300)
+
+getwd()
+dat <- read_excel("./data/ReproducibleResearchParticipants2017-090517.xlsx")
+
+onames <- names(dat)
+
+dat <- dat %>% 
+  purrr::set_names("created", "first_name", "last_name", "email", "role",
+                   "academic", "school", "department", "division", "sessions",
+                   "comments")
+
+dat$role %>% table(exclude=NULL)
+dat$academic %>% table
+dat$school %>% table
+dat$department %>% table
+dat$division %>% table
+
+dat %>% #select(sessions)
+  mutate(workshop1 = str_detect(sessions, "Workshop 1"),
+         workshop2 = str_detect(sessions, "Workshop 2")) %>%
+  summarize(`Reporting` = n_perc0(workshop1),
+            `Version Control` = n_perc0(workshop2))
+
+
+dat %>% select(contains("name"), email, role, comments) %>% filter(!is.na(comments)) %>% .$comments
+
+
+
+
